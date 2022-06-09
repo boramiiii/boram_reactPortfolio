dd35d72a4ccffce85d9d09633af4f45abda2d995
boramiiii <b_ram1227@naver.com>
2022-06-09 09:01:13 +0900

    커밋

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 ReactDOM.render(
-	<HashRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<App />
-	</HashRouter>,
+	</BrowserRouter>,
 	document.getElementById('root')
 );
